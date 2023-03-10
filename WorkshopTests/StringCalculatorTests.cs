d2f2a95a2e6b7f5b6c1ba8fa8b213d96fa4b9c07
szpejnad <szpejnad@P21914>
2023-03-10 17:50:52 +0100

    Add project files.

---
+++ WorkshopTests/StringCalculatorTests.cs
@@ -0,0 +1,96 @@
+using Workshp2023Tests;
+
+namespace WorkshopTests
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void EmptyStringReturnsZero()
+        {
+            int result = StringCalculator.Calculate("");
+            Assert.Equal(0, result);
+        }
+
+        [Theory]
+        [InlineData("12", 12)]
+        [InlineData("0", 0)]
+        [InlineData("121", 121)]
+        [InlineData("48", 48)]
+        public void StringWithNumberReturnsItsValue(string str, int expected)
+        {
+            int result = StringCalculator.Calculate(str);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("12,12", 24)]
+        [InlineData("1,0", 1)]
+        //[InlineData("12,-12", 0)]
+        //[InlineData("-8,-4", -12)]
+        //[InlineData("8,-4", 4)]
+        public void TwoNumberWithCommaSeparatorReturnSum(string str, int expected)
+        {
+            int result = StringCalculator.Calculate(str);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("12\n12", 24)]
+        [InlineData("1\n0", 1)]
+        //[InlineData("12\n-12", 0)]
+        //[InlineData("-8\n-4", -12)]
+        //[InlineData("8\n-4", 4)]
+        public void TwoNumberWithNewLineSeparatorReturnSum(string str, int expected)
+        {
+            int result = StringCalculator.Calculate(str);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("12\n12\n12", 36)]
+        [InlineData("1\n0,1", 2)]
+        //[InlineData("12,-12,3", 3)]
+        //[InlineData("-8,-4\n2\n2", -8)]
+        //[InlineData("8\n-4,1,1", 6)]
+        public void ManyNumberWithNewLineOrCommaSeparatorReturnSum(string str, int expected)
+        {
+            int result = StringCalculator.Calculate(str);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("12\n12\n12", 36)]
+        [InlineData("1001", 0)]
+        [InlineData("1002\n1", 1)]
+        [InlineData("1000", 1000)]
+        [InlineData("1000, 1, 2", 1003)]
+        public void NumberBiggerThanThousandIgnored(string str, int expected)
+        {
+            int result = StringCalculator.Calculate(str);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("12\n-12\n12")]
+        [InlineData("-1001")]
+        [InlineData("1002\n-1")]
+        [InlineData("-10")]
+        [InlineData("1000, -1, 2")]
+        public void ThrowErrorIfThereIsNegativeNumber(string str)
+        {
+            Assert.Throws<ArgumentException>(() => StringCalculator.Calculate(str));
+        }
+
+        [Theory]
+        [InlineData("//g\n12g12", 24)]
+        [InlineData("//X\n0X1X3", 4)]
+        [InlineData("//X\n12", 12)]
+        [InlineData("//X\n7X0X0X0X0X0X1", 8)]
+        [InlineData("//X\n", 0)]
+        public void CodeAtTheBeginningOfStringChangeSeparator(string str, int expected)
+        {
+            int result = StringCalculator.Calculate(str);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Workshp2023Tests/StringCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshp2023Tests
+{
+    public class StringCalculator
+    {
+        public static int Calculate(string str)
+        {
+            if (String.IsNullOrEmpty(str) || str.Equals("//X\n")) return 0;
+
+            List<char> separtors = new List<char> { ',', '\n' };
+
+            if (str.StartsWith("//") && char.IsLetter(str[2]) && str[3] == '\n' )
+            {
+                separtors = new List<char> { str[2] };
+                str = str.Substring(4);
+            }
+
+            int num = 0;
+            {
+                string[] numbers = str.Split(separtors.ToArray());
+                num = numbers.Sum(n => {
+                    if (int.Parse(n) < 0) throw new ArgumentException();
+                    if (int.Parse(n) > 1000) return 0;
+                    return int.Parse(n); 
+                });
+            }
+
+            return num;
+        }
+    }
+}
